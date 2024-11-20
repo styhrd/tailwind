685eebbc8adb338be54a87189f2578eb3e834385
styhrd <juanmiguelcaparas76@gmail.com>
2024-11-20 13:13:25 +0800

    Env Setup

---
+++ nike/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='text-white-400 px-5 py-2 bg-blue-400'>Hello World</p>
+    </>
+  )
+}
+
+export default App
